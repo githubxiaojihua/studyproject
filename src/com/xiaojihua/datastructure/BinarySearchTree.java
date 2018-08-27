174f4ab96b8bb932972a8afcb65ffb276e2543be
xiaojihua <2542213767@qq.com>
2018-08-27 14:21:32 +0800

    test

--- src/com/xiaojihua/datastructure/BinarySearchTree.java
+++ src/com/xiaojihua/datastructure/BinarySearchTree.java
@@ -247,8 +247,8 @@ public class BinarySearchTree<AnyType extends Comparable> {
 	public static void main( String [ ] args )
 	{
 		BinarySearchTree<Integer> t = new BinarySearchTree<>( );
-		final int NUMS = 4013;
-		final int GAP  =   36;
+		final int NUMS = 4000;
+		final int GAP  =   37;
 
 		System.out.println( "Checking... (no more output means success)" );
 
