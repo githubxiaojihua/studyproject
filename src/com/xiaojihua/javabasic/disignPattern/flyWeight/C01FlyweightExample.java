2a08bf41f5c03c73ef930b8ee848c8e1c7730778
xjh <240971505@qq.com>
2019-05-27 23:32:24 +0800

    20190527

--- src/com/xiaojihua/javabasic/disignPattern/flyWeight/C01FlyweightExample.java
+++ src/com/xiaojihua/javabasic/disignPattern/flyWeight/C01FlyweightExample.java
@@ -9,6 +9,7 @@ import java.util.WeakHashMap;
  * FlyWeight是通过与其他相似对象共享尽可能多的数据来最小化内存使用的一种对象。
  * 享元对象的部分属性和状态是共享的，而且这些对象一般都存储在外部
  * 的数据结构中，在使用的时候临时传递他们给对应的引用。
+ * 比如本里中不变的部分是CoffeeFlavor的name，
  *
  * 为了能在客户和线程之间安全的共享，FlyWeight对象必须是不变的（immutable)。
  * FlyWeith应该是value object(简单说value obejct的值一旦确定就不会改变了），
@@ -59,6 +60,7 @@ class CoffeeFlavor{
 
     public static CoffeeFlavor intern(String name){
         synchronized(CACHE){
+            //如果cache有name对应的value则返回，如果没有则新增
             return CACHE.computeIfAbsent(name,CoffeeFlavor::new);
         }
     }
@@ -76,6 +78,7 @@ class CoffeeFlavor{
 interface Order {
     void serve();
 
+    //静态默认方法，与之对应的是普通的静态方法用关键字default
     static Order of(String flavourName, int tableNumber) {
         CoffeeFlavor flavour = CoffeeFlavor.intern(flavourName);
         return () -> System.out.println("Serving " + flavour + " to table " + tableNumber);
