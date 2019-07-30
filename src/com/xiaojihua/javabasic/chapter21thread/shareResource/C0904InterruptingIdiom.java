4a52cb7f4a5f7fbe57c4ccc19e049918bb433b25
xiaojihua <2542213767@qq.com>
2019-07-30 11:04:25 +0800

    20190730

--- src/com/xiaojihua/javabasic/chapter21thread/shareResource/C0904InterruptingIdiom.java
+++ src/com/xiaojihua/javabasic/chapter21thread/shareResource/C0904InterruptingIdiom.java
@@ -12,6 +12,22 @@ import java.util.concurrent.TimeUnit;
  * 状态已经复位了）
  *
  * 下面的例子中演示了，处于阻塞时候的中断以及出于非阻塞时候的中断
+ *
+ * 中断线程调用的两种途径：
+ * 在任务的run方法中经常会使用到while循环，通常结束while循环
+ * 或者是结束当前进程的时候需要通过不同的方式，当程序存在阻塞的时候
+ * 通过调用interrupt()方法，线程会抛出InteruptedException异常
+ * 通过捕获异常来中断阻塞的线程（sleep等能够抛出InterruptedException的
+ * 方法可以通过如上方法实现中断，但是IO阻塞和synchronized阻塞是不能通过
+ * 这种方式中断的只能结束程序或者关闭底层资源）。
+ * 但是如果线程没有存在阻塞想通过interrupt()方法来结束的话，需要
+ * 判断线程的中断状态，因为在调用interrupt()方法后会设置线程的中断状态为true
+ * 通过Thread.interrupted()可以判断线程中断标志，但是注意当调用了一遍
+ * Thread.interrupted()后线程的中断标志即复位，如果想要重新判断的话需要
+ * 将Thread.interrupted()的值存储起来。
+ *
+ * 本例可以通过设置main线程中的sleep时长和Block3中point1中的sleep时长
+ * 来测试阻塞时中断和非阻塞时中断的不同
  */
 
 /**
@@ -42,9 +58,26 @@ class Block3 implements Runnable{
             while(!Thread.interrupted()){
                 NeedsCleanup n1 = new NeedsCleanup(1);
                 try {
+                    /*
+                        本任务通过两个不同的point来测试阻塞和非阻塞情况下
+                        如何退出run方法
+                        下面两句是第一个point，实现了阻塞，如果程序运行到喜爱案
+                        这两句处于阻塞的情况下，外部调用线程的interrpt()方法后
+                        这里会抛出异常。使用try finnally包裹起来
+                        保证资源进行了释放
+                     */
                     System.out.println("sleeping");
                     TimeUnit.SECONDS.sleep(1);
+
                     NeedsCleanup n2 = new NeedsCleanup(2);
+                    /*
+                        下面是第二个point，通过模拟复杂计算模拟了非阻塞情况，
+                        此时在外部调用interrupt方法并不会抛出异常而是，会执行完成
+                        剩余的语句，当到达新一轮while的时候通过
+                        !Thread.interrupted()判断是否线程的中断状态被设置了
+                        如果被设置了那么就不在进行while循环了。
+                        同样如果有待释放的资源的话需要使用try finally包裹起来
+                     */
                     try{
                         //模拟复杂计算
                         System.out.println("Calculating");
@@ -76,7 +109,7 @@ public class C0904InterruptingIdiom {
     public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(new Block3());
         thread.start();
-        TimeUnit.MILLISECONDS.sleep(6000);
+        TimeUnit.MILLISECONDS.sleep(1001);
         thread.interrupt();
     }
 }
